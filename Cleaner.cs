2787630f171dccc9709f272a126a590194eebf9b
AstroBurgers <astroburgers1@gmail.com>
2025-01-02 11:04:40 -0600

    init commit

---
+++ Cleaner.cs
@@ -0,0 +1,36 @@
+﻿namespace DirectoryCleaner;
+
+internal enum CleanType
+{
+    EFolders,
+    EFiles,
+    EAll
+}
+
+internal class Cleaner(
+    CleanType cleanType,
+    List<string> excludedFiles,
+    List<string> excludedFolders,
+    string maxFileSize = "",
+    string minFileSize = "",
+    int minFolderDepth = 0,
+    int maxFolderDepth = 0)
+{
+    public Cleaner(CleanType cleanType, List<string> excludedFolders, int minFolderDepth = 0, int maxFolderDepth = 0) : this(cleanType, null, excludedFolders, null, null, minFolderDepth, maxFolderDepth)
+    {
+    }
+
+    public Cleaner(CleanType cleanType, List<string> excludedFiles, string maxFileSize = "", string minFileSize = "") : this(cleanType, excludedFiles, null, maxFileSize, minFileSize, 0, 0)
+    {
+    }
+    
+    internal CleanType CleanType { get; set; } = cleanType;
+    internal List<string> CleanedFiles { get; set; }
+    internal List<string> CleanedFolders { get; set; }
+    internal List<string> ExcludedFiles { get; set; } = excludedFiles;
+    internal List<string> ExcludedFolders { get; set; } = excludedFolders;
+    internal string MaxFileSize { get; set; } = maxFileSize;
+    internal string MinFileSize { get; set; } = minFileSize;
+    internal int MinFolderDepth { get; set; } = minFolderDepth;
+    internal int MaxFolderDepth { get; set; } = maxFolderDepth;
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DirectoryCleaner;
+
+internal class Program()
+{
+    internal static string? dirString;
+    internal static void Main()
+    {
+        Console.WriteLine("Welcome to the directory cleaner!");
+        Console.WriteLine(@"Provided path must be in format: C:\Users\User\...\Directory");
+        do
+        {
+            Console.Write("Please enter the path to the directory you would like to clean:");
+            dirString = Console.ReadLine();
+            if(!Path.Exists(dirString)) {
+                Console.WriteLine("Invalid path format // Path does not exist. Please try again.");
+            }
+        }while(!Directory.Exists(dirString) || string.IsNullOrEmpty(dirString) || !Path.Exists(dirString));
+
+        Console.ReadLine();
+    }
+}
